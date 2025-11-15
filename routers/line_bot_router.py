f2ba52b7692fdea7d402363ba61a532d516cb07a
Weston <lovemow1121@gmail.com>
2025-11-16 01:25:27 +0800

    line bot router hook add log

--- routers/line_bot_router.py
+++ routers/line_bot_router.py
@@ -1,5 +1,6 @@
 import json
 from fastapi import APIRouter, Request
+from pyfk import get_logger
 
 from library.dao import LineChat, LineChatDao
 
@@ -8,10 +9,13 @@ router = APIRouter(
     tags=["line-bot"]
 )
 
+log = get_logger()
+
 @router.post("/hook")
 async def hook(request: Request):
     body = await request.body()
     chat_dict = json.loads(body)
+    log.info(chat_dict)
     event = chat_dict["events"][0]
     message = event["message"]
     source = event["source"]
